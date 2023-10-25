d4ca10e3cafcef093de8f6d20740f55f68d5369c
QuyNguyen <quynv.mr@gmail.com>
2023-10-26 00:12:56 +0700

    add

---
+++ src/app/appointment-list/appointment-list.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import {Appointment} from '../models/appointment';
+
+@Component({
+  selector: 'app-appointment-list',
+  templateUrl: './appointment-list.component.html',
+  styleUrls: ['./appointment-list.component.css']
+})
+export class AppointmentListComponent {
+  newAppointmentTitle : string = "";
+  newAppointmentDate : Date = new Date();
+
+  appointments : Appointment[] = []
+
+  addAppointment () {
+
+  }
+
+}
